ec5ee3a5f12eebe2be1263fcff21fa00df36e424
melrpm <medelorbe@outlook.com>
2022-09-13 14:11:21 -0400

    change to delta

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
   }
   return (
     <div className={styles.container}>
-      <Image src = '/santodomingomotorslogo.png' alt="imgnotfound" width={75} height={75}/>
+      <Image src = '/icon-delta.png' alt="imgnotfound" width={75} height={75}/>
       <Image src='/landcruiser.png' alt='imgnotfound'width={350} height={210}/>
       <Image src='/frame.png' alt='imgnotfound'  width={100} height={100}/>
       <h1 style={{color:'black'}}>Toyota Landcruiser</h1>
@@ -36,12 +36,12 @@ export default function Home() {
       </div>
       
       <div style={{width:'100%', maxWidth:'300px', display:'flex', flexDirection:'row', justifyContent:'space-between', marginTop:'50px', color:'#000287'}}>
-        <a href='tel:809-540-3800' target="_blank" rel="noopener noreferrer">Llamar</a>
-        <a href='https://api.whatsapp.com/send?phone=18295215151&text=%C2%A1Hola!%20Quiero%20hacer%20cita%20de%20mantenimiento%20%E2%98%91%EF%B8%8F' target="_blank" rel="noopener noreferrer">Agendar Cita</a>
+        <a href='tel:8096203000' target="_blank" rel="noopener noreferrer">Llamar</a>
+        <a href='https://api.whatsapp.com/send?phone=+18492580765&text=Hola!%20me%20interesa%20una%20cita' target="_blank" rel="noopener noreferrer">Agendar Cita</a>
       </div>
       <div style={{width:'100%', maxWidth:'300px', display:'flex', flexDirection:'row', justifyContent:'space-between', marginTop:'20px', color:'#000287'}}>
-        <a href='mailto:email@santodomingomotors.com.do' target="_blank" rel="noopener noreferrer">Enviar Correo</a>
-        <a href='https://www.santodomingomotors.com.do/' target="_blank" rel="noopener noreferrer">Visitar pagina web</a>
+        <a href='mailto:email@deltacomercial.com.do' target="_blank" rel="noopener noreferrer">Enviar Correo</a>
+        <a href='https://deltacomercial.com.do/' target="_blank" rel="noopener noreferrer">Visitar pagina web</a>
       </div>
     </div>
   )
